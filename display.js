d1de1d7be0e8f1e6d3a12e817bca5fa12eb8da9d
charles mancuso <charlesmancuso66@yahoo.com>
2020-04-28 21:36:18 -0700

    Add drawrectangle function

--- display.js
+++ display.js
@@ -2,6 +2,10 @@ const Display = function(canvas) {
   this.buffer = document.createElement('canvas').getContext('2d'),
   this.context = canvas.getContext('2d');
 
+  this.drawRectancle = function(x, y, width, height, color) {
+
+  }
+  
   this.renderColor = function(color) {
     this.buffer.fillStyle = color;
     this.buffer.fillRect(0, 0, this.buffer.canvas.width, this.buffer.canvas.height);
