6ff2461b30eb14f6b679a601fd15b7bd9f3d6638
charles mancuso <charlesmancuso66@yahoo.com>
2020-04-29 21:27:17 -0700

    Modify update function in main

--- main.js
+++ main.js
@@ -21,8 +21,14 @@ window.addEventListener('load', function(event) {
   };
 
   const update = function() {
+    if(controller.left.active) {game.world.player.moveLeft()}
+    if(controller.right.active) {game.world.player.moveRight()}
+    if(controller.up.active) {
+      game.world.player.jump()
+      controller.up.active = false;
+    }
     game.update();
-  }
+  };
 
   const controller = new Controller();
   const display = new Display(document.querySelector('canvas'));
