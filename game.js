8031b28a5fb524f8c4ac71f901f6d4070c54b637
charles mancuso <charlesmancuso66@yahoo.com>
2020-04-29 21:15:43 -0700

    Add jump function as well as change colors if you jump

--- game.js
+++ game.js
@@ -54,3 +54,17 @@ Game.Player = function(x, y) {
   this.x = 100;
   this.y = 50;
 }
+
+Game.Player.prototype = {
+  constructor: Game.Player,
+  jump: function() {
+    if(!this.jumping) {
+      this.color = "#" + Math.floor(Math.random() * 16777216).toString(16);
+      if(this.color.length != 7) {
+        this.color = this.color.slice(0, 1) + "0" + this.color.slice(1, 6);
+      }
+      this.jumping = true;
+      this.velocity_y -= 20;
+    }
+  }
+}
